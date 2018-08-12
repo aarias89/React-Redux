4149beffded8257982bd4075e132a6cd49323392
[Andres] <andresar11@gmail.com>
2018-08-12 18:19:16 -0400

    Create action that will be responsible for making AJAX requests. Add ROOT_URL, and set up variables for API call.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
-const API_KEY= "594da4a5f67a0a1420a9264004d57971";
+const API_KEY = "594da4a5f67a0a1420a9264004d57971";
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+//As convention, make a single varaible that hold Action type.
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+//action creators always have to return an Action, and every action
+//has to have a type
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+
+  return {
+    type: FETCH_WEATHER
+  }
+}
